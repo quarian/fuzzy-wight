bc0cad5b14a1c9fe97fac3e308c28507a4e26ce3
Nurmela Miro <nurmelm1@cc.hut.fi>
2013-02-28 15:52:53 +0200

    added max acceleration functinality, basic throwing things

--- Shaker/src/com/authorwjf/Main.java
+++ Shaker/src/com/authorwjf/Main.java
@@ -14,6 +14,10 @@ import android.widget.TextView;
 public class Main extends Activity implements SensorEventListener {
 	
 	private float mLastX, mLastY, mLastZ;
+	private float maxX, maxY, maxZ;
+	float t = (float)0.1;
+	float g = (float)9.81;
+	float result = (float)0.0;
 	private boolean mInitialized;
 	private SensorManager mSensorManager;
     private Sensor mAccelerometer;
@@ -45,11 +49,21 @@ public class Main extends Activity implements SensorEventListener {
 		// can be safely ignored for this demo
 	}
 
+	private float throw_phone(float x, float y, float z) {
+		float time = 2 * ((t * y)/g);
+		float distance = 1/2 * x * time * time;
+		return distance;
+	}
+	
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		TextView tvX= (TextView)findViewById(R.id.x_axis);
 		TextView tvY= (TextView)findViewById(R.id.y_axis);
 		TextView tvZ= (TextView)findViewById(R.id.z_axis);
+		TextView tvX_max= (TextView)findViewById(R.id.x_max);
+		TextView tvY_max= (TextView)findViewById(R.id.y_max);
+		TextView tvZ_max= (TextView)findViewById(R.id.z_max);
+		TextView res= (TextView)findViewById(R.id.result);
 		ImageView iv = (ImageView)findViewById(R.id.image);
 		float x = event.values[0];
 		float y = event.values[1];
@@ -58,9 +72,17 @@ public class Main extends Activity implements SensorEventListener {
 			mLastX = x;
 			mLastY = y;
 			mLastZ = z;
+			maxX = x;
+			maxY = y;
+			maxZ = z;
+			result = throw_phone(x, y, z);
 			tvX.setText("0.0");
 			tvY.setText("0.0");
 			tvZ.setText("0.0");
+			tvX_max.setText(Float.toString(x));
+			tvY_max.setText(Float.toString(y));
+			tvZ_max.setText(Float.toString(z));
+			res.setText("0.0");
 			mInitialized = true;
 		} else {
 			float deltaX = Math.abs(mLastX - x);
@@ -72,9 +94,27 @@ public class Main extends Activity implements SensorEventListener {
 			mLastX = x;
 			mLastY = y;
 			mLastZ = z;
+			float temp;
+			temp = throw_phone(mLastX, mLastY, mLastZ);
 			tvX.setText(Float.toString(deltaX));
 			tvY.setText(Float.toString(deltaY));
 			tvZ.setText(Float.toString(deltaZ));
+			if (mLastX > maxX) {
+				maxX = mLastX;
+				tvX.setText(Float.toString(mLastX));
+			}
+			if (mLastY > maxY) {
+				maxY = mLastY;
+				tvX.setText(Float.toString(mLastY));
+			}
+			if (mLastZ > maxZ) {
+				maxZ = mLastZ;
+				tvX.setText(Float.toString(mLastZ));
+			}
+			if (temp > result) {
+				result = temp;
+				res.setText(Float.toString(result));
+			}		
 			iv.setVisibility(View.VISIBLE);
 			if (deltaX > deltaY) {
 				iv.setImageResource(R.drawable.horizontal);
